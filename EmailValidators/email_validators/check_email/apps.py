8dea21da0b1cbd2a8220d020b848372046cfaa9b
Игорь Луковец <igor.lukavets@gmail.com>
2022-03-04 15:04:06 +0300

    lab2

--- EmailValidators/email_validators/check_email/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CheckEmailConfig(AppConfig):
-    name = 'check_email'
--- EmailValidators/email_validators/check_email/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.email_send, name='email_send')
-]
