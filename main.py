1a05bd61701d45e13b651703a1f2ecb7f00c5320
Evgeni Bonda <maelstrom2016@mail.ru>
2022-03-18 14:50:15 +0300

    lab2

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-import re
-import io
-
-with io.open('fl1.md', encoding='utf-8') as f1:
-    rd = f1.read()
-
-def search_for_md(r_d):
-
-    r_d = re.sub(r'\>\b', r'<quote>', r_d)
-    r_d = re.sub(r'\b\>\n', r'</quote>', r_d)
-    r_d = re.sub(r'\#{6}\s*\b', r'<h6>', r_d)
-    r_d = re.sub(r'\#{6}', r'</h6>', r_d)
-    r_d = re.sub(r'\#{5}\s*\b', r'<h5>', r_d)
-    r_d = re.sub(r'\#{5}', r'</h5>', r_d)
-    r_d = re.sub(r'\#{4}\s*\b', r'<h4>', r_d)
-    r_d = re.sub(r'\#{4}', r'</h4>', r_d)
-    r_d = re.sub(r'\#{3}\s*\b', r'<h3>', r_d)
-    r_d = re.sub(r'\#{3}', r'</h3>', r_d)
-    r_d = re.sub(r'\#{2}\s*\b', r'<h2>', r_d)
-    r_d = re.sub(r'\#{2}', r'</h2>', r_d)
-    r_d = re.sub(r'\#\s*\b', r'<h1>', r_d)
-    r_d = re.sub(r'\#', r'</h1>', r_d)
-    r_d = re.sub(r'\~{2}\s*\b', r'<string>', r_d)
-    r_d = re.sub(r'\~{2}', r'</string>', r_d)
-    r_d = re.sub(r'\*{2}\s*\b', r'<ss>', r_d)
-    r_d = re.sub(r'\*{2}', r'</ss>', r_d)
-    r_d = re.sub(r'\`\s*\b', r'<code>', r_d)
-    r_d = re.sub(r'\`\n', r'</code>', r_d)
-    r_d = re.sub(r'\d\.\s', '<list>', r_d)
-    r_d = re.sub(r'\b\n', '</list>', r_d)
+IDENTIFIES = []
 
 
-    #print(r_d)
-    with open('fl2.html', 'w') as f:
-        w_d = f.write(r_d)
+def find(value):
+    for i in range(len(IDENTIFIES)):
+        if IDENTIFIES[i] == value:
+            return True, i + 1
+    return False, len(IDENTIFIES)
 
-search_for_md(rd)
+
+def insert(value):
+    count = 0
+    for i in range(len(IDENTIFIES)):
+        if IDENTIFIES[i] > value:
+            IDENTIFIES.insert(i, value)
+            return count + 1
+        count += 1
+    IDENTIFIES.append(value)
+    return count
+
+
+with open('text.txt', 'r') as fp:
+    for item in fp:
+        print(insert(item.strip()), item.strip(), IDENTIFIES)
+
+print(IDENTIFIES)
+print(find(input()))
