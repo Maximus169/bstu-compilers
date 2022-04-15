c10c634428cb2bc4a5bd4f225660db33455d1460
SkipperCRLSS <99957323+SkipperCRLSS@users.noreply.github.com>
2022-04-15 14:33:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,107 @@
 import re
-import io
-
-with io.open('fl1.md', encoding='utf-8') as f1:
-    rd = f1.read()
-
-def search_for_md(r_d):
-
-    r_d = re.sub(r'\>\b', r'<quote>', r_d)
-    r_d = re.sub(r'\b\>\n', r'</quote>', r_d)
-    r_d = re.sub(r'\#{6}\s*\b', r'<h6>', r_d)
-    r_d = re.sub(r'\#{6}', r'</h6>', r_d)
-    r_d = re.sub(r'\#{5}\s*\b', r'<h5>', r_d)
-    r_d = re.sub(r'\#{5}', r'</h5>', r_d)
-    r_d = re.sub(r'\#{4}\s*\b', r'<h4>', r_d)
-    r_d = re.sub(r'\#{4}', r'</h4>', r_d)
-    r_d = re.sub(r'\#{3}\s*\b', r'<h3>', r_d)
-    r_d = re.sub(r'\#{3}', r'</h3>', r_d)
-    r_d = re.sub(r'\#{2}\s*\b', r'<h2>', r_d)
-    r_d = re.sub(r'\#{2}', r'</h2>', r_d)
-    r_d = re.sub(r'\#\s*\b', r'<h1>', r_d)
-    r_d = re.sub(r'\#', r'</h1>', r_d)
-    r_d = re.sub(r'\~{2}\s*\b', r'<string>', r_d)
-    r_d = re.sub(r'\~{2}', r'</string>', r_d)
-    r_d = re.sub(r'\*{2}\s*\b', r'<ss>', r_d)
-    r_d = re.sub(r'\*{2}', r'</ss>', r_d)
-    r_d = re.sub(r'\`\s*\b', r'<code>', r_d)
-    r_d = re.sub(r'\`\n', r'</code>', r_d)
-    r_d = re.sub(r'\d\.\s', '<list>', r_d)
-    r_d = re.sub(r'\b\n', '</list>', r_d)
 
 
-    #print(r_d)
-    with open('fl2.html', 'w') as f:
-        w_d = f.write(r_d)
+KEY_SYMBOLS = {
+    '(': 'Знак открывающей скобки',
+    ')': 'Знак закрывающей скобки',
+    ',': 'Запятая',
+}
 
-search_for_md(rd)
+
+def is_id(line):
+    return {
+        'result': not re.fullmatch(r'\d*[.,]\d+', line) is None,
+        'name': 'Число с плавающей точкой',
+        'priority': 3,
+        'data': line,
+    }
+
+
+def is_symbol_const(line):
+    return {
+        'result': not re.fullmatch("'.'", line) is None,
+        'name': 'Число',
+        'priority': 1,
+        'data': line
+    }
+
+
+def is_string_const(line):
+    return {
+        'result': not re.fullmatch(r'".*"', line) is None,
+        'name': 'Строковая константа',
+        'priority': 4,
+        'data': line,
+    }
+
+
+def is_key_symbol(line):
+    if line in KEY_SYMBOLS.keys():
+        return {
+            'result': True,
+            'name': KEY_SYMBOLS.get(line),
+            'priority': 2,
+            'data': line,
+        }
+    else:
+        return {
+            'result': False,
+            'name': 'Ключевой символ',
+            'priority': 0,
+            'data': line
+        }
+
+
+CHECK_CODE = [
+    is_id,
+    is_string_const,
+    is_symbol_const,
+    is_key_symbol,
+]
+
+
+def check_tag(tag):
+    res = []
+    is_true = False
+    for func in CHECK_CODE:
+        result = func(tag)
+        res.append(result)
+        is_true = is_true or result.get('result')
+    return {'result': is_true, 'data': res}
+
+
+with open('program.txt', 'r') as fp:
+    for line in fp:
+        line = line.strip()
+        start = 0
+        end = 1
+        while True:
+            tag = line[start: end]
+            if tag == ';':
+                if start == len(line) - 1:
+                    break
+                else:
+                    start += 1
+                    end += 1
+            if tag == ' ':
+                start += 1
+                end += 1
+                continue
+            result = check_tag(tag)
+
+            if not result.get('result'):
+                result = check_tag(tag[: len(tag) - 1])
+                priority = -1
+                info = None
+                for item in result.get('data'):
+                    if item.get('result') and item.get('priority') > priority:
+                        priority = item.get('priority')
+                        info = item
+                if info is None:
+                    end += 1
+                    continue
+                print(info.get('name'), info.get('data'))
+                start = end - 1
+            else:
+                end += 1
+
